6d509588a40529ce9e7fff543d7f53e9c381d9ee
Yuqi Li <ziyeqinghan@gmail.com>
2014-11-06 10:48:03 +0800

    Candy

---
+++ Candy.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int candy(vector<int> &ratings) {
+        vector<int> up(ratings.size()), down(ratings.size());
+        int i;
+        for(i = 1; i < ratings.size(); i++) {
+            if(ratings[i] > ratings[i-1])
+                up[i] = up[i-1]+1;
+            else
+                up[i] = 0;
+        }
+        for(i = ratings.size()-2; i >= 0; i--) {
+            if(ratings[i] > ratings[i+1])
+                down[i] = down[i+1]+1;
+            else
+                down[i] = 0;
+        }
+        int ans = 0;
+        for(i = 0; i < ratings.size(); i++) 
+            ans += max(up[i], down[i]) + 1;
+        return ans;
+        
+    }
+};
