a48aeb00c144c8be9abc5cb0d6a376c0c45296e9
Yuqi Li <ziyeqinghan@gmail.com>
2014-11-20 21:21:28 +0800

    Sqrt(x)

---
+++ Sqrt(x).cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int sqrt(int x) {
+        if(x < 0)
+            return -1;
+        if(x == 0)
+            return 0;
+        int m, l = 1, r = x;
+        while(l <= r) {
+            m = l + (r-l)/2;    // 防止int越界
+            if(m > x / m)       // 防止int越界
+                r = m-1;
+            else
+                l = m+1;
+        }
+        return r;             //舍掉小数位
+    }
+};
