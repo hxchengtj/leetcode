c6cf8ff9c19b403e792cb89bd1718246a7381943
Yuqi Li <ziyeqinghan@gmail.com>
2014-12-01 21:10:46 +0800

    modified Binary Tree Inorder Traversal

--- Binary Tree Inorder Traversal.cpp
+++ Binary Tree Inorder Traversal.cpp
@@ -7,6 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+ /*
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode *root) {
@@ -17,6 +18,29 @@ public:
         ans.push_back(root->val);
         v = inorderTraversal(root->right);
         ans.insert(ans.end(), v.begin(), v.end());
+        return ans;
+    }
+};
+*/
+// iteratively
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode *root) {
+        stack<TreeNode *> s;
+        vector<int> ans;
+        TreeNode *t = root;
+        while(t || !s.empty()) {
+            if(t) {
+                s.push(t);
+                t = t->left;
+            }
+            else {
+                t = s.top();
+                ans.push_back(t->val);
+                s.pop();
+                t = t->right;
+            }
+        }
         return ans;
     }
 };
