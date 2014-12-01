d314e3b747e30a2f4a964d54877fc5020c77d5db
Yuqi Li <ziyeqinghan@gmail.com>
2014-12-01 16:41:01 +0800

    Insert Interval

---
+++ Insert Interval.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
+        int i;
+        vector<Interval> ans;
+        for(i = 0; i < intervals.size() && intervals[i].start <= newInterval.end; i++) 
+            if(intervals[i].end >= newInterval.start) {
+                newInterval.start = min(newInterval.start, intervals[i].start);
+                newInterval.end = max(newInterval.end, intervals[i].end);
+            }
+            else
+                ans.push_back(intervals[i]);
+        ans.push_back(newInterval);
+        for(; i < intervals.size(); i++)
+            ans.push_back(intervals[i]);
+        return ans;
+    }
+};
