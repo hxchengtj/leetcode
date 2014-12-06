6969734ce94bf7813f9f372be6bf90720f7d0749
Yuqi Li <ziyeqinghan@gmail.com>
2014-12-06 16:40:10 +0800

    modified Find Peak Element

--- Find Peak Element.cpp
+++ Find Peak Element.cpp
@@ -1,3 +1,5 @@
+// 写错了不知为何过了=。=
+/*
 class Solution {
 public:
     int findPeakElement(const vector<int> &num) {
@@ -8,6 +10,24 @@ public:
                 l = m+1;
             else
                 r = m-1;
+        }
+        return l;
+    }
+};
+*/
+
+class Solution {
+public:
+    int findPeakElement(const vector<int> &num) {
+        int n = num.size(), l = 0, r = n-1, m;
+        while(l <= r) {
+            m = (l+r)/2;
+            if(m+1 < n && num[m] < num[m+1])
+                l = m+1;
+            else if(m-1 >= 0 && num[m] < num[m-1])
+                r = m-1;
+            else 
+                return m;
         }
         return l;
     }
