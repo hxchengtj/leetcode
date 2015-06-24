ce1a8ca2360207443d379351f7a407810b9c32f6
ziyeqinghan <ziyeqinghan@ziyeqinghandeMacBook-Air.local>
2015-06-24 20:07:37 +0800

    Maximal Square

---
+++ Maximal Square.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        if(matrix.size() == 0 || matrix[0].size() == 0)
+            return 0;
+        int m = matrix.size(), n = matrix[0].size();
+        
+        int f[m][n];
+        int maxf = 0;
+        for(int i = 0; i < n; i++) {
+            if(matrix[0][i] == '1') {
+                f[0][i] = 1;
+                maxf = 1;
+            }
+            else
+                f[0][i] = 0;
+        }
+        for(int i = 0; i < m; i++) {
+            if(matrix[i][0] == '1') {
+                f[i][0] = 1;
+                maxf = 1;
+            }
+            else
+                f[i][0] = 0;
+        }
+        for(int i = 1; i < m; i++)
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][j] == '1') {
+                    f[i][j] = min(min(f[i-1][j-1], f[i][j-1]), f[i-1][j])+1;
+                    maxf = max(maxf, f[i][j]);
+                }
+                else
+                    f[i][j] = 0;
+            }
+        return maxf*maxf;
+    }
+};
