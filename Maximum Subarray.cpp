45c8b8eb7ce9c3adfe85e70b92630f9e98ecde52
ziyeqinghan <ziyeqinghan@ziyeqinghandeMacBook-Air.local>
2015-07-03 20:13:54 +0800

    Maximum Subarray

--- Maximum Subarray.cpp
+++ Maximum Subarray.cpp
@@ -14,3 +14,18 @@ public:
         return maxsum;
     }
 };
+
+//redo
+//version2
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+        int sum = nums[0], ret = nums[0];
+        for(int i = 1; i < nums.size(); i++) {
+            sum = max(sum+nums[i], nums[i]);
+            ret = max(ret, sum);
+        }
+        return ret;
+    }
+};
