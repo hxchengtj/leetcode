91ccba280d564b3a3d0ee879f55435fbf878ab70
ziyeqinghan <ziyeqinghan@ziyeqinghandeMacBook-Air.local>
2015-07-03 18:25:36 +0800

    redo Next Permutation

--- Next Permutation.cpp
+++ Next Permutation.cpp
@@ -21,3 +21,20 @@ public:
         }
     }
 };
+
+//redo
+//version2
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n-2, j;
+        while(i >= 0 && nums[i] >= nums[i+1]) i--;
+        if(i >= 0) {
+            j = n-1;
+            while(nums[j] <= nums[i]) j--;
+            swap(nums[j], nums[i]);
+        }
+        reverse(nums.begin()+i+1, nums.end());
+    }
+};
