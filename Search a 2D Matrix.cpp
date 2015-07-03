32c6199b375efdda0c865ecb4f96025a716f7386
ziyeqinghan <ziyeqinghan@ziyeqinghandeMacBook-Air.local>
2015-07-03 16:46:07 +0800

    redo Search a 2D Matrix

--- Search a 2D Matrix.cpp
+++ Search a 2D Matrix.cpp
@@ -29,3 +29,25 @@ public:
         return false;
     }
 };
+
+//redo
+//version2
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(matrix.size() == 0 || matrix[0].size() == 0)
+            return false;
+        int m = matrix.size(), n = matrix[0].size();
+        int l = 0, r = m*n;
+        while(l < r) {
+            int mid = l + (r-l)/2;
+            if(matrix[mid/n][mid%n] < target)
+                l = mid+1;
+            else
+                r = mid;
+        }
+        if(l < m*n && matrix[l/n][l%n] == target)
+            return true;
+        return false;
+    }
+};
