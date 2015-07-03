bba4628625c621c4137d06a0a3d9b58753b077af
ziyeqinghan <ziyeqinghan@ziyeqinghandeMacBook-Air.local>
2015-07-03 20:02:14 +0800

    Merge Intervals

--- Merge Intervals.cpp
+++ Merge Intervals.cpp
@@ -25,3 +25,38 @@ public:
         return ans;
     }
 };
+
+//redo
+//version2
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    vector<Interval> merge(vector<Interval>& intervals) {
+        vector<Interval> v;
+        if(intervals.size() == 0)
+            return v;
+        sort(intervals.begin(), intervals.end(), [](const Interval &a, const Interval &b) {
+            return a.start < b.start;
+        });
+        Interval a(intervals[0].start, intervals[0].end);
+        for(int i = 1; i < intervals.size(); i++) {
+            if(intervals[i].start <= a.end)
+                a.end = max(a.end, intervals[i].end);
+            else {
+                v.push_back(a);
+                a.start = intervals[i].start;
+                a.end = intervals[i].end;
+            }
+        }
+        v.push_back(a);
+        return v;
+    }
+};
