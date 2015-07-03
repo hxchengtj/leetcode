ede1c728f400ec67bc8ada204ad267f2ea0a42b1
ziyeqinghan <ziyeqinghan@ziyeqinghandeMacBook-Air.local>
2015-07-03 15:32:59 +0800

    Valid Palindrome

--- Valid Palindrome.cpp
+++ Valid Palindrome.cpp
@@ -28,7 +28,8 @@ public:
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int l = 0, r = s.size()-1;
+        //int l = 0, r = s.size()-1;
+        int l = 0, r = (int)s.size()-1;
         while(l < r) {
             if(!isalnum(s[l])) l++;
             else if(!isalnum(s[r])) r--;
