7c94eda5f7962563ae957a62a511bf0ad4a0226b
ziyeqinghan <ziyeqinghan@ziyeqinghandeMacBook-Air.local>
2015-07-04 20:59:37 +0800

    Search in Rotated Sorted Array

--- Search in Rotated Sorted Array.cpp
+++ Search in Rotated Sorted Array.cpp
@@ -26,3 +26,30 @@ public:
         return -1;
     }
 };
+
+//redo
+//version2
+class Solution {
+public:
+    int __search(vector<int>& nums, int target, int l, int r) {
+        while(l < r) {
+            int m = l + (r-l)/2;
+            if(nums[m] < target) l = m+1;
+            else if(nums[m] > target) r = m;
+            else return m;
+        }
+        return -1;
+    }
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size();
+        while(l < r) {
+            int m = l + (r-l)/2;
+            if(nums[m] < nums[l]) r = m;
+            else {
+                l = m+1;
+                if(l < r && nums[l] < nums[m]) break;
+            }
+        }
+        return max(__search(nums, target, 0, l), __search(nums, target, l, nums.size()));
+    }
+};
