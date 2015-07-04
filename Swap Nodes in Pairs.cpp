e2065f3198107a585a1baf900d1c9029ba425670
ziyeqinghan <ziyeqinghan@ziyeqinghandeMacBook-Air.local>
2015-07-04 15:48:05 +0800

    Swap Nodes in Pairs

--- Swap Nodes in Pairs.cpp
+++ Swap Nodes in Pairs.cpp
@@ -16,3 +16,31 @@ public:
         return HEAD->next;
     }
 };
+
+//redo
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode HEAD(0);
+        HEAD.next = head;
+        ListNode* p = &HEAD;
+        while(p->next && p->next->next) {
+            ListNode* q = p->next;
+            ListNode* qn = q->next;
+            ListNode* a = qn->next;
+            p->next = qn;
+            qn->next = q;
+            q->next = a;
+            p = q;
+        }
+        return HEAD.next;
+    }
+};
