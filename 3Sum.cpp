7e35c14307cf00388b5903eab4e1dae30f533c20
ziyeqinghan <ziyeqinghan@ziyeqinghandeMacBook-Air.local>
2015-07-04 11:50:44 +0800

    3Sum

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -31,4 +31,43 @@ public:
         }
         return ans;
     }
+
+    //redo
+    //version2
+    class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+        if(nums.size() < 3)
+            return ans;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<int> v(3, 0);
+        for(int i = 0; i < n-2; i++) {
+            int j = i+1, k = n-1;
+            while(j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum < 0) {
+                    j++;
+                    while(j < k && nums[j] == nums[j-1]) j++;
+                }
+                else if(sum > 0) {
+                    k--;
+                    while(j < k && nums[k] == nums[k+1]) k--;
+                }
+                else {
+                    v[0] = nums[i]; v[1] = nums[j]; v[2] = nums[k];
+                    ans.push_back(v);
+                    j++;
+                    while(j < k && nums[j] == nums[j-1]) j++;
+                    k--;
+                    while(j < k && nums[k] == nums[k+1]) k--;
+                }
+            }
+            i++;
+            while(i < n-2 && nums[i] == nums[i-1]) i++;
+            i--;
+        }
+    }
+};
 };
