0ebf4e55f4b5e680065cf4c83dd4a4a3e12eefa4
ziyeqinghan <ziyeqinghan@ziyeqinghandeMacBook-Air.local>
2015-07-05 10:38:06 +0800

    First Missing Positive

--- First Missing Positive.cpp
+++ First Missing Positive.cpp
@@ -19,3 +19,20 @@ public:
         return n+1;
     }
 };
+
+//redo
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+        while(i < n){
+            if(nums[i] >= 1 && nums[i] <= n && nums[nums[i]-1] != nums[i]) 
+                swap(nums[nums[i]-1], nums[i]);
+            else i++;
+        }
+        for(int i = 0; i < n; i++)
+            if(nums[i] != i+1) return i+1;
+        return n+1;
+    }
+};
