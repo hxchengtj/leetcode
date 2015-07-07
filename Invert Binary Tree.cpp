d709878abee6dcecbcb048046341a96364c496e7
ziyeqinghan <ziyeqinghan@ziyeqinghandeMacBook-Air.local>
2015-07-07 14:58:17 +0800

    Invert Binary Tree

--- Invert Binary Tree.cpp
+++ Invert Binary Tree.cpp
@@ -18,3 +18,17 @@ public:
         return root;
     }
 };
+
+//redo
+//version2
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == NULL)
+            return NULL;
+        root->left = invertTree(root->left);
+        root->right = invertTree(root->right);
+        swap(root->left, root->right);
+        return root;
+    }
+};s
