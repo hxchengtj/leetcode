0f73bbec6fd4017980e9e5a48c9b3f476f64eeeb
ziyeqinghan <ziyeqinghan@ziyeqinghandeMacBook-Air.local>
2015-07-07 14:49:14 +0800

    Max Points on a Line

--- Max Points on a Line.cpp
+++ Max Points on a Line.cpp
@@ -21,3 +21,43 @@ public:
         return ans;
     }
 };
+
+
+//redo
+/**
+ * Definition for a point.
+ * struct Point {
+ *     int x;
+ *     int y;
+ *     Point() : x(0), y(0) {}
+ *     Point(int a, int b) : x(a), y(b) {}
+ * };
+ */
+class Solution {
+public:
+    int maxPoints(vector<Point>& points) {
+        int ans = 0;
+        sort(points.begin(), points.end(), [](const Point & a, const Point & b) {
+           if(a.x != b.x) return a.x < b.x;
+           else return a.y < b.y;
+        });
+        
+        for(int i = 0; i < points.size(); i++) {
+            unordered_map<double, int> m;
+            int j = i+1;
+            while(j < points.size() && points[i].x == points[j].x && points[i].y == points[j].y)
+                j++;
+            int k = j-i;
+            for(;j < points.size(); j++) {
+                double detax = points[j].x-points[i].x;
+                double detay = points[j].y-points[i].y;
+                if(detay == 0) m[double(INT_MAX)-double(INT_MIN)+1]++;
+                else m[detax/detay]++;
+            }
+            ans = max(ans, k);
+            for(auto &p:m)
+                ans = max(ans, k+p.second);
+        }
+        return ans;
+    }
+};
