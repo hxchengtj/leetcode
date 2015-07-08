fe3e861957a775aecc295d6359fd8fd4f3dd28ba
ziyeqinghan <ziyeqinghan@ziyeqinghandeMacBook-Air.local>
2015-07-08 15:36:07 +0800

    Best Time to Buy and Sell Stock IV

--- Best Time to Buy and Sell Stock IV.cpp
+++ Best Time to Buy and Sell Stock IV.cpp
@@ -25,3 +25,41 @@ public:
         return sell[k-1];
     }
 };
+
+//version2
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        vector<int> profit;
+        stack<pair<int, int>> stk;
+        int p = 0, v = 0;
+        while(v < n) {
+            while(v+1 < n && prices[v+1] <= prices[v]) v++;
+            p = v;
+            while(p+1 < n && prices[p] <= prices[p+1]) p++;
+            while(!stk.empty()) {
+                if(prices[v] <= prices[stk.top().first])
+                    profit.push_back(prices[stk.top().second]-prices[stk.top().first]);
+                else if(prices[p] >= prices[stk.top().second]) {
+                    profit.push_back(prices[stk.top().second]-prices[v]);
+                    v = stk.top().first;
+                }
+                else break;
+                stk.pop();
+            }
+            stk.push(make_pair(v, p));
+            v = p+1;
+        }
+        while(!stk.empty()) {
+            profit.push_back(prices[stk.top().second]-prices[stk.top().first]);
+            stk.pop();
+        }
+        k = min(k, (int)profit.size());
+        nth_element(profit.begin(), profit.begin() + k, profit.end(), greater<int>());
+        int ans = 0;
+        for(int i = 0; i < k; i++)
+            ans += profit[i];
+        return ans;
+    }
+};
