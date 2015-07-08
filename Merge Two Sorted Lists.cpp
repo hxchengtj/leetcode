4a8d2acd4252eb34fc8d5d7c459ead51f8c1813f
ziyeqinghan <ziyeqinghan@ziyeqinghandeMacBook-Air.local>
2015-07-08 21:47:35 +0800

    Merge Two Sorted Lists

--- Merge Two Sorted Lists.cpp
+++ Merge Two Sorted Lists.cpp
@@ -38,3 +38,27 @@ public:
         return l;
     }
 };
+
+
+//version2
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode HEAD(0);
+        ListNode *p = &HEAD;
+        while(l1 && l2) {
+            if(l1->val < l2->val) { 
+                p->next = l1;
+                l1 = l1->next;
+            }
+            else {
+                p->next = l2;
+                l2 = l2->next;
+            }
+            p = p->next;
+        }
+        if(l1) p->next = l1;
+        else if(l2) p->next = l2;
+        return HEAD.next;
+    }
+};
