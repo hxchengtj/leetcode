68f29918be21bff3027893f09b012e61eb2931ba
ziyeqinghan <ziyeqinghan@ziyeqinghandeMacBook-Air.local>
2015-07-08 20:19:29 +0800

    Maximum Depth of Binary Tree

--- Maximum Depth of Binary Tree.cpp
+++ Maximum Depth of Binary Tree.cpp
@@ -37,3 +37,12 @@ public:
         return depth[b-1];
     }
 };
+
+//version2
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL) return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
