9a70645d5d1ff866b6a9f5bfd9d2499bdebd0a7c
ziyeqinghan <ziyeqinghan@ziyeqinghandeMacBook-Air.local>
2015-07-08 22:12:57 +0800

    Add Two Numbers

--- Add Two Numbers.cpp
+++ Add Two Numbers.cpp
@@ -34,3 +34,27 @@ public:
         return l;
     }
 };
+
+//version2
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode HEAD(0);
+        ListNode* p = &HEAD;
+        int c = 0;
+        while(l1 || l2 || c) {
+            if(l1) {
+                c += l1->val;
+                l1 = l1->next;
+            }
+            if(l2) {
+                c += l2->val;
+                l2 = l2->next;
+            }
+            p->next = new ListNode(c%10);
+            p = p->next;
+            c = c/10;
+        }
+        return HEAD.next;
+    }
+};
