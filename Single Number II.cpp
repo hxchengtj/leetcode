5dd2a1654c9db14dfe96f3db1e8b32653c19f770
ziyeqinghan <ziyeqinghan@ziyeqinghandeMacBook-Air.local>
2015-07-09 21:12:53 +0800

    Single Number II

--- Single Number II.cpp
+++ Single Number II.cpp
@@ -22,3 +22,17 @@ public:
         return a;
     }
 };
+
+//version2
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int a = 0, b = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            int t = (~a & b & nums[i]) | (a & ~b & ~nums[i]);
+            b = (~a & ~b & nums[i]) | (~a & b & ~nums[i]);
+            a = t;
+        }
+        return b;
+    }
+};
