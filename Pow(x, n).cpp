adc2e0ab6bf9880f5e83ceeab47710de64555239
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-11 18:23:35 +0800

    Pow(x, n)

--- Pow(x, n).cpp
+++ Pow(x, n).cpp
@@ -18,3 +18,20 @@ public:
         return t;
     }
 };
+
+
+//version2
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1;
+        double a = x;
+        int m = n >= 0? n: -(n+1);
+        for(int i = 0; i < 31; i++) {
+            if( (m&(1<<i)) != 0) ans *= a;
+            a = a*a;
+        }
+        ans = n >= 0? ans:1/ans/x;
+        return ans;
+    }
+};
