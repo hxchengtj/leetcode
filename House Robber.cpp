e1ce33d4134a5c984b3b701f890ac1f20919f9ba
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-13 17:01:22 +0800

    House Robber

--- House Robber.cpp
+++ House Robber.cpp
@@ -11,3 +11,18 @@ public:
         return f[nums.size()-1];
     }
 };
+
+//version2
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.size() == 0)    return 0;
+        if(nums.size() == 1)    return nums[0];
+        int f[2];
+        f[0] = nums[0];
+        f[1] = max(f[0], nums[1]);
+        for(int i = 2; i < nums.size(); i++)
+            f[i%2] = max(nums[i]+f[i%2], f[(i+1)%2]);
+        return f[(nums.size()-1)%2];
+    }
+};
