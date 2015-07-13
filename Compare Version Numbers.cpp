ce976efb9881fbc3a1cd4c4f424c011ba065f7a8
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-13 17:39:26 +0800

    Compare Version Numbers

--- Compare Version Numbers.cpp
+++ Compare Version Numbers.cpp
@@ -28,3 +28,25 @@ public:
         else return compareVersion(s1, s2);
     }
 };
+
+//version2
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int m = version1.size(), n = version2.size();
+        int l1 = 0, l2 = 0;
+        while(l1 < m || l2 < n){
+            int r1 = version1.find(".", l1);
+            int r2 = version2.find(".", l2);
+            if(r1 == -1) r1 = m;
+            if(r2 == -1) r2 = n;
+            int v1 = l1 == r1 ? 0 : stoi(version1.substr(l1, r1-l1));
+            int v2 = l2 == r2 ? 0 : stoi(version2.substr(l2, r2-l2));
+            if(v1 < v2) return -1;
+            else if(v1 > v2) return 1;
+            l1 = min(r1+1, m);
+            l2 = min(r2+1, n);
+        }
+        return 0;
+    }
+};
