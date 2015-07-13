87eb8534df4d2fdd400d2ac2f660d8f08899a004
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-13 19:28:28 +0800

    Unique Binary Search Trees II

--- Unique Binary Search Trees II.cpp
+++ Unique Binary Search Trees II.cpp
@@ -25,3 +25,30 @@ public:
         
     }
 };
+
+//version2
+class Solution {
+public:
+    vector<TreeNode*> dfs(int l, int r) {
+        vector<TreeNode*> v;
+        if(l > r) {
+            v.push_back(NULL);
+            return v;
+        }
+        for(int i = l; i <= r; i++) {
+            vector<TreeNode*> lv = dfs(l, i-1);
+            vector<TreeNode*> rv = dfs(i+1, r);
+            for(auto ln:lv)
+                for(auto rn:rv) {
+                    TreeNode* t = new TreeNode(i);
+                    t->left = ln;
+                    t->right = rn;
+                    v.push_back(t);
+                }
+        }
+        return v;
+    }
+    vector<TreeNode*> generateTrees(int n) {
+        return dfs(1, n);
+    }
+};
