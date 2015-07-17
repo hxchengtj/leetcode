0994767e12c5155d0722ce1c4d6d7cf23cfa31ed
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-17 18:36:05 +0800

    Longest Palindromic Substring

--- Longest Palindromic Substring.cpp
+++ Longest Palindromic Substring.cpp
@@ -20,3 +20,38 @@ public:
         return s.substr(l, r-l);
     }
 };
+
+//version2
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string str("#");
+        for(auto c : s) {
+            str.push_back(c);
+            str.push_back('#');
+        }
+        
+        int n = str.size();
+        int f[n];
+        memset(f, 0, sizeof(f));
+        int m = 0, mx = 0;
+        for(int i = 0; i < n; i++) {
+            f[i] = i-m < mx ? min(f[2*m-i], mx-i): 1;
+            int j = f[i]+i;
+            while(j < n && 2*i-j >= 0 && str[j] == str[2*i-j]) j++;
+            f[i] = j-i;
+            if(j > mx) {
+                mx = j;
+                m = i;
+            }
+            
+        }
+        
+        int k = distance(f, max_element(f, f+n));
+        string ans;
+        for(int i = k-f[k]+1; i < f[k]+k; i++) {
+            if(str[i] != '#') ans.push_back(str[i]);
+        }
+        return ans;
+    }
+};
