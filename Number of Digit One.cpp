8ce02a19e9248b251f5b8c4cbd958770e4370b1d
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-22 13:14:40 +0800

    Number of Digit One

---
+++ Number of Digit One.cpp
@@ -0,0 +1,17 @@
+//review
+class Solution {
+public:
+    int countDigitOne(int n) {
+        if(n <= 0) return 0;
+        int ans = 0;
+        long long t = 10;
+        while(n >= t/10) {
+            ans += (n/t)*(t/10);
+            int a = (n/(t/10))%10;
+            if(a == 1) ans += (n%(t/10))+1;
+            else if(a > 1) ans += t/10;
+            t *= 10;
+        }
+        return ans;
+    }
+};
