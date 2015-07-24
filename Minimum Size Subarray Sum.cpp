0c29760711402ef9852f8538742085a7021d7a5b
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-24 11:27:02 +0800

    Minimum Size Subarray Sum

--- Minimum Size Subarray Sum.cpp
+++ Minimum Size Subarray Sum.cpp
@@ -23,3 +23,19 @@ public:
     }
 
 };
+
+//redo
+int minSubArrayLen(int s, vector<int>& nums) {
+  if(s <= 0) return 0;
+  int n = nums.size(), ans = 0;
+  int l = 0, r = 0, sum = 0;
+  while(l < n) {
+      if(sum < s && r < n) sum += nums[r++];
+        else {
+              if(sum >= s) ans = ans == 0 ? r-l : min(ans, r-l);
+                  sum -= nums[l++];
+        }
+  }
+  return ans;
+}
+
