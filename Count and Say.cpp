04d2d0a8c59a65e01ae697b39f0da6a43bb01a45
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-24 10:22:59 +0800

    Count and Say

--- Count and Say.cpp
+++ Count and Say.cpp
@@ -9,18 +9,41 @@ public:
             s2 = "";
             k = 1;
             for(j = 1; j < s1.length(); j++) {
-                if(s1[j] == s1[j-1]) 
+                if(s1[j] == s1[j-1])
                     k++;
                 else {
                     c = k + '0';
                     s2 = s2 + c + s1[j-1];
                     k = 1;
                 }
-            }   
+            }
             c = k + '0';
             s2 = s2 + c + s1[j-1];
         }
         return s2;
     }
-    
+
 };
+
+//redo
+string count(int n) {
+  string s;
+  if(n <= 0) return s;
+  s = "1";
+  for(int i = 1; i < n; i++) {
+    char c = s[0];
+    int k = 1;
+    string t;
+    for(int j = 1; j <= s.size(); j++) {
+      if(j == s.size() || s[j] != c) {
+        t.append(to_string(k)+c);
+        k = 1;
+        c = s[j];
+      }
+      else k++;
+    }
+    s = t;
+  }
+  return s;
+}
+
