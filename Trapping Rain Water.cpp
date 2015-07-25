b0bb032f36aa4b74ad2d3dc93f752f4fa3a23a1d
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-25 20:10:24 +0800

    Trapping Rain Water

--- Trapping Rain Water.cpp
+++ Trapping Rain Water.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int trap(int A[], int n) {
         int i, j, maxa = A[0], num = 0, sum[n], ans = 0;
-        sum[0] = A[0]; 
+        sum[0] = A[0];
         for(i = 1; i < n; i++) {
             if(maxa < A[i]) {
                 num = i;
@@ -22,7 +22,7 @@ public:
             s.pop();
             j = s.top();
             ans += (j-i-1) * A[i] - sum[j-1] + sum[i];
-        }        
+        }
         for(i = num+1;i < n; i++) {
             while(!s.empty() && A[s.top()] < A[i])
                 s.pop();
@@ -59,3 +59,22 @@ public:
         return ans;
     }
 };
+
+//version2
+//redo
+int trap(vector<int>& height) {
+  int ans = 0;
+  int n = height.size();
+  int l = 0, r = n-1, maxl = 0, maxr = 0;
+  while(l <= r) {
+    if(height[l] < height[r]) {
+      ans += max(0, maxl-height[l]);
+      maxl = max(maxl, height[l++]);
+    } else {
+      ans += max(0, maxr-height[r]);
+      maxr = max(maxr, height[r--]);
+    }
+  }
+  return ans;
+}
+
