1986abf50ff582b5d428d770651486b46a128cf5
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-26 20:37:29 +0800

    Palindrome Partitioning II

--- Palindrome Partitioning II.cpp
+++ Palindrome Partitioning II.cpp
@@ -13,10 +13,34 @@ public:
         g[1] = 0;
         for(i = 2; i <= n; i++) {
             g[i] = INT_MAX;
-            for(j = 0; j < i; j++) 
+            for(j = 0; j < i; j++)
                 if(f[j][i])
                     g[i] = min(g[i], g[j]+1);
         }
         return g[n];
     }
 };
+
+
+//redo
+int minCut(string s) {
+  int n = s.size();
+  bool isP[n+1][n+1];
+  for(int j = 0; j <= n; j++)
+    for(int i = 0; j+i<= n; i++) {
+      if(j < 2) isP[i][j] = true;
+      else isP[i][j] = (s[i] == s[i+j-1]) && isP[i+1][j-2];
+    }
+  int f[n+1];
+  f[0] = 0;
+  for(int i = 1; i <= n; i++) {
+    if(isP[0][i]) f[i] = 0;
+    else {
+      f[i] = i;
+      for(int j = 1; j < i;j++)
+        if(isP[j][i-j]) f[i] = min(f[i], f[j]+1);
+    }
+  }
+  return f[n];
+}
+
