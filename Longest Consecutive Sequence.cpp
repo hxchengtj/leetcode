620b5c2e66f678675ed5f05484dba8e0ae052dd5
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-26 18:18:05 +0800

    Longest Consecutive Sequence

--- Longest Consecutive Sequence.cpp
+++ Longest Consecutive Sequence.cpp
@@ -8,12 +8,35 @@ public:
         for(int &n:num)
             m.insert(n);
         for(i = 0; i < num.size(); i++) {
-            for(j = 0, k = num[i], iter = m.find(num[i]); iter != m.end(); j++, k--, iter = m.find(k)) 
+            for(j = 0, k = num[i], iter = m.find(num[i]); iter != m.end(); j++, k--, iter = m.find(k))
                 m.erase(iter);
-            for(k = num[i]+1, iter = m.find(k); iter != m.end(); j++, k++, iter = m.find(k)) 
+            for(k = num[i]+1, iter = m.find(k); iter != m.end(); j++, k++, iter = m.find(k))
                 m.erase(iter);
             ans = max(ans, j);
         }
         return ans;
     }
 };
+
+//redo
+int longestConsecutive(vector<int>& nums) {
+  unordered_set<int> s(nums.begin(), nums.end());
+  int ans = 0;
+  for(auto a:nums) {
+    if(s.find(a) != s.end()) {
+      int m = 0, b = a;
+      while(s.find(b) != s.end()) {
+        m++;
+        s.erase(b++);
+      }
+      b = a-1;
+      while(s.find(b) != s.end()) {
+        m++;
+        s.erase(b--);
+      }
+      ans = max(m, ans);
+    }
+  }
+  return ans;
+}
+
