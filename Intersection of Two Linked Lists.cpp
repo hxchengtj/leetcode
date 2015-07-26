e07776a7953ba76afce8b4cc4d23cbde961ecb2d
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-26 18:25:57 +0800

    Intersection of Two Linked Lists

--- Intersection of Two Linked Lists.cpp
+++ Intersection of Two Linked Lists.cpp
@@ -17,6 +17,20 @@ public:
                 l = l1;
         }
         return l;
-            
+
     }
 };
+
+
+//redo
+ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+  ListNode* p = headA, *q = headB;
+  while(p != q) {
+    if(p == NULL) p = headB;
+    else p = p->next;
+    if(q == NULL) q = headA;
+    else q = q->next;
+  }
+  return p;
+}
+
