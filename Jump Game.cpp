18bd67a60c3394f0bf45f95db50d660cd5118b01
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-27 16:43:51 +0800

    Jump Game

--- Jump Game.cpp
+++ Jump Game.cpp
@@ -2,8 +2,20 @@ class Solution {
 public:
     bool canJump(int A[], int n) {
         int i, j = 0;
-        for(i = 0; i <= j && j < n; i++) 
+        for(i = 0; i <= j && j < n; i++)
             j = max(j, i+A[i]);
         return j >= (n-1);
     }
 };
+
+//redo
+bool canJump(vector<int>& nums) {
+  int len = 0, n = nums.size();
+  for(int i = 0; i < n; i++) {
+    if(len < i) return false;
+    len = max(len, i+nums[i]);
+  }
+  return true;
+}
+
+
