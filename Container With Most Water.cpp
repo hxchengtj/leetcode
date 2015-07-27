201c5af899f624b237165f84a87ec2699bdc64a0
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-27 18:28:24 +0800

    Container With Most Water

--- Container With Most Water.cpp
+++ Container With Most Water.cpp
@@ -16,3 +16,22 @@ public:
         return ans;
     }
 };
+
+
+//redo
+int maxArea(vector<int>& height) {
+  int l = 0, r = height.size()-1;
+  int ans = 0;
+  while(l < r){
+    if(height[l] < height[r]) {
+      ans = max(ans, height[l]*(r-l));
+      l++;
+    }
+    else {
+      ans = max(ans, height[r]*(r-l));
+      r--;
+    }
+  }
+  return ans;
+}
+
