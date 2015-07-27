479008edd6159e212900913182d39b5082ef0c33
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-27 18:19:05 +0800

    Linked List Cycle

--- Linked List Cycle.cpp
+++ Linked List Cycle.cpp
@@ -14,3 +14,17 @@ public:
         return false;
     }
 };
+
+
+//redo
+bool hasCycle(ListNode* head) {
+  if(head == NULL || head->next == NULL) return false;
+  ListNode* p = head, *q = head->next;
+  while(q && p != q) {
+    p = p->next;
+    q = q->next;
+    if(q) q = q->next;
+  }
+  return q != NULL;
+}
+
