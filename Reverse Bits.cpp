f96e82e41f6a8e20538f1f31bd27a02484b11586
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-28 14:59:07 +0800

    Reverse Bits

--- Reverse Bits.cpp
+++ Reverse Bits.cpp
@@ -10,3 +10,14 @@ public:
         return c;
     }
 };
+
+
+//redo
+uint32_t reverseBits(uint32_t n) {
+  int ans = 0;
+  for(int i = 0; i < 32; i++) {
+    ans = (ans << 1) | (n & 1);
+    n = n >> 1;
+  }
+}
+
