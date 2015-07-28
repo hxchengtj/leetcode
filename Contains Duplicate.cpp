3351d9c6a6fd7bd4b58240205ae60f1941c7e5ed
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-28 14:16:19 +0800

    Contains Duplicate

--- Contains Duplicate.cpp
+++ Contains Duplicate.cpp
@@ -10,3 +10,14 @@ public:
         return false;
     }
 };
+
+
+//redo
+bool containsDuplicate(vector<int>& nums) {
+  unordered_set<int> s;
+  for(auto a:nums){
+    if(s.find(a) != s.end()) return true;
+    s.insert(a);
+  }
+  return false;
+}
