ad161e947c787991c62ba54050b52f7cfad62403
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-28 14:47:25 +0800

    Palindrome Linked List

--- Palindrome Linked List.cpp
+++ Palindrome Linked List.cpp
@@ -5,7 +5,7 @@ public:
         ListNode* p = head, *q = head->next;
         while(q) {
             q = q->next;
-            if(q) { 
+            if(q) {
                 q = q->next;
                 p = p->next;
             }
@@ -24,7 +24,7 @@ public:
         }
         prev->next = reverse_list(head2);
         return true;
-        
+
     }
     ListNode* reverse_list(ListNode* head) {
         ListNode *p = head, *nextp = head->next;
@@ -38,3 +38,44 @@ public:
         return p;
     }
 };
+
+
+//redo
+bool isPalindrome(ListNode* head) {
+  if(head == NULL || head->next == NULL) return true;
+  ListNode* p = head, *q = head->next;
+  while(q) {
+    q = q->next;
+    if(q) {
+      q = q->next;
+      p = p->next;
+    }
+  }
+  ListNode* m = p;
+  ListNode* head1 = reverseList(m->next);
+  m->next = NULL;
+  p = head, q = head1;
+  bool flag = true;
+  while(p && q) {
+    if(p->val != q->val) {
+      flag = false;
+      break;
+    }
+    p = p->next; q = q->next;
+  }
+  m->next = reverseList(head1);
+  return flag;
+}
+
+ListNode* reverseList(ListNode* head) {
+  ListNode *node = head, *nextnode = head->next;
+  head->next = NULL;
+  while(nextnode) {
+    ListNode* t = nextnode->next;
+    nextnode->next = node;
+    node = nextnode;
+    nextnode = t;
+  }
+  return node;
+}
+
