3f4cea4de67df911b45b52fbee0a172c0cbb4211
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-28 14:32:31 +0800

    Path Sum II

--- Path Sum II.cpp
+++ Path Sum II.cpp
@@ -25,3 +25,24 @@ public:
         v.pop_back();
     }
 };
+
+
+//redo
+vector<vector<int>> ans;
+vector<int> v;
+void dfs(TreeNode* root, int sum) {
+  v.push_back(root->val);
+  if(root->left == NULL && root->right == NULL) {
+    if(sum == root->val) ans.push_back(v);
+  }
+  if(root->left) dfs(root->left, sum-root->val);
+  if(root->right) dfs(root->right, sum-root->val);
+  v.pop_back();
+}
+vector<vector<int>> pathSum(TreeNode* root, int sum) {
+  ans.clear(); v.clear();
+  if(root)
+    dfs(root, sum);
+  return ans;
+}
+
