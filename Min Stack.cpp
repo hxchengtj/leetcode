70694e00303c722fc5c0b10a02f753617a6acdbf
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-29 11:27:11 +0800

    Min Stack

--- Min Stack.cpp
+++ Min Stack.cpp
@@ -15,10 +15,36 @@ public:
     }
 
     int top() {
-        return s.top();   
+        return s.top();
     }
 
     int getMin() {
         return q.top();
     }
 };
+
+
+//redo
+class MinStack {
+  stack<int> stk;
+  stack<int> minstk;
+  public:
+  void push(int x) {
+    if(minstk.empty() || minstk.top() >= x) minstk.push(x);
+    stk.push(x);
+  }
+
+  void pop() {
+    if(minstk.top() == stk.top()) minstk.pop();
+    stk.pop();
+  }
+
+  int top() {
+    return stk.top();
+  }
+
+  int getMin() {
+    return minstk.top();
+  }
+};
+
