f0744351c0f4203c86389b8ed5c286a1e9c2e548
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-29 11:49:41 +0800

    Remove Element

--- Remove Element.cpp
+++ Remove Element.cpp
@@ -23,3 +23,12 @@ public:
         return j;
     }
 };
+
+//redo2
+int removeElement(vector<int>& nums, int val) {
+  int j = 0;
+  for(int i = 0; i < nums.size(); i++)
+    if(nums[i] != val) nums[j++] = nums[i];
+  return j;
+}
+
