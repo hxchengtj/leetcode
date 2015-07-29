9f31c03aa54598b9da44bf10de5ba6d45cf52ac8
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-29 20:27:06 +0800

    Remove Duplicates from Sorted Array II

--- Remove Duplicates from Sorted Array II.cpp
+++ Remove Duplicates from Sorted Array II.cpp
@@ -4,7 +4,7 @@ public:
         if(!n)
             return 0;
         int i, length = 1, dup = 1;
-        for(i = 1 ; i < n; i++) 
+        for(i = 1 ; i < n; i++)
             if(A[i] != A[i-1]) {
                 dup = 1;
                 A[length] = A[i];
@@ -18,3 +18,19 @@ public:
         return length;
     }
 };
+
+//redo
+int removeDuplicates(vector<int>& nums) {
+  if(nums.size() == 0) return 0;
+  int j = 1, count = 1;
+  for(int i = 1; i < nums.size(); i++) {
+    if(nums[i] == nums[i-1]) {
+      if(++count <= 2) nums[j++] = nums[i];
+    } else {
+      nums[j++] = nums[i];
+      count = 1;
+    }
+  }
+  return j;
+}
+
