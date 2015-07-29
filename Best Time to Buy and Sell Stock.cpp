829ac725e4cf7349afc090a36fd1247b0d76905a
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-29 19:46:05 +0800

    Best Time to Buy and Sell Stock

--- Best Time to Buy and Sell Stock.cpp
+++ Best Time to Buy and Sell Stock.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxProfit(vector<int> &prices) {
         int minp = 0,maxprofit = 0;
-        if(prices.size()) 
+        if(prices.size())
             minp = prices[0];
         for(int i = 0;i < prices.size(); i++) {
             minp = min(minp, prices[i]);
@@ -11,3 +11,15 @@ public:
         return maxprofit;
     }
 };
+
+
+//redo
+int maxProfit(vector<int>& prices) {
+  int hold = INT_MIN, sell = 0;
+  for(auto &p:prices) {
+    hold = max(hold, -p);
+    sell = max(sell, hold+p);
+  }
+  return sell;
+}
+
