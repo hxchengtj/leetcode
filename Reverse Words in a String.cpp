c1bb16fbd284df15ebc72ec43fcca9891de81d44
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-30 16:32:38 +0800

    Reverse Words in a String

--- Reverse Words in a String.cpp
+++ Reverse Words in a String.cpp
@@ -6,10 +6,10 @@ public:
         string t, a;
         for(i = 0; i < s.length(); i++) {
             if(s[i] == ' ') {
-                if(t.length() > 0) { 
+                if(t.length() > 0) {
                     if(a.length())
                         a = t + " "+ a;
-                    else 
+                    else
                         a = t;
                 }
                 t = "";
@@ -17,10 +17,10 @@ public:
             else
                 t += s[i];
         }
-        if(t.length() > 0){ 
+        if(t.length() > 0){
             if(a.length())
                 a = t + " "+ a;
-            else 
+            else
                 a = t;
         }
         s = a;
@@ -47,3 +47,20 @@ public:
         s.resize(k);
     }
 };
+
+//version2
+//redo
+void reverse(string& s) {
+  reverse(s.begin(), s.end());
+  int n = s.size(), l = 0, r, k = 0;
+  while(l < n) {
+    while(l < n && s[l] == ' ') l++;
+    r = l;
+    while(r < n && s[r] != ' ') r++;
+    reverse(s.begin()+l, s.begin()+r);
+    if(k > 0 && r - l > 0) s[k++] = ' ';
+    while(l < r) s[k++] = s[l++];
+  }
+  s.resize(k);
+}
+
