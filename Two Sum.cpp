b3ff6cf72cea513e41dce370df255bf32a0a8042
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-30 19:46:06 +0800

    Two Sum

--- Two Sum.cpp
+++ Two Sum.cpp
@@ -16,3 +16,16 @@ public:
         return v;
     }
 };
+
+
+//redo
+vector<int> twoSum(vector<int>& nums, int target) {
+  unordered_map<int, int> m;
+  for(int i = 0; i < nums.size(); i++) {
+    auto it = m.find(target-nums[i]);
+    if(it != m.end()) return {it->second+1, i+1};
+    m[nums[i]] = i;
+  }
+  return {};
+}
+
