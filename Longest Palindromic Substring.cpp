d9e53421b7de3aa6f9b04c7064cc1d49c8398f78
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-30 15:45:34 +0800

    Longest Palindromic Substring

--- Longest Palindromic Substring.cpp
+++ Longest Palindromic Substring.cpp
@@ -30,7 +30,7 @@ public:
             str.push_back(c);
             str.push_back('#');
         }
-        
+
         int n = str.size();
         int f[n];
         memset(f, 0, sizeof(f));
@@ -44,9 +44,9 @@ public:
                 mx = j;
                 m = i;
             }
-            
+
         }
-        
+
         int k = distance(f, max_element(f, f+n));
         string ans;
         for(int i = k-f[k]+1; i < f[k]+k; i++) {
@@ -55,3 +55,32 @@ public:
         return ans;
     }
 };
+
+//version2
+//redo
+string longestPalindromic(string s) {
+  string str = "#";
+  for(auto c:s) {
+    str.push_back(c);
+    str.push_back('#');
+  }
+  int n = str.size();
+  vector<int> f(n, 0);
+  int i = 0, j = 1;
+  while(j < n) {
+    if(i + f[i] < j || f[2*i-j]+j == i+f[i]) {
+      int k = i + f[i] + 1;
+      while(k < n && 2*j-k >= 0 && str[k] == str[2*j-k]) k++;
+      f[j] = k-j-1;
+      i = j;
+    }
+    else f[j] = min(f[2*i-j], i+f[i]-j);
+    j++;
+  }
+  i = distance(f.begin(), max_element(f.begin(), f.end()));
+  string ans;
+  for(j = i-f[i]; j <= i+f[i]; j++)
+    if(str[j] != '#') ans.push_back(str[j]);
+  return ans;
+}
+
