6d67f1f32953f687b743a80e332f53c9c2afee58
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-31 14:53:55 +0800

    Valid Palindrome

--- Valid Palindrome.cpp
+++ Valid Palindrome.cpp
@@ -23,7 +23,6 @@ public:
     }
 };
 
-//redo 
 //version2
 class Solution {
 public:
@@ -42,3 +41,20 @@ public:
         return true;
     }
 };
+
+//version2
+//redo
+bool isPalindrome(string s) {
+  int i = 0, j = s.size()-1;
+  while(i < j) {
+    if(!isalnum(s[i])) i++;
+    else if(!isalnum(s[j])) j--;
+    else if(tolower(s[i]) != tolower(s[j])) return false;
+    else {
+      i++;
+      j--;
+    }
+  }
+  return true;
+}
+
