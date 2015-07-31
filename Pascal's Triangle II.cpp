27564555cb82cb11ce6cdce8b39868f515d2627d
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-31 19:48:23 +0800

    Pascal's Triangle II

--- Pascal's Triangle II.cpp
+++ Pascal's Triangle II.cpp
@@ -15,3 +15,15 @@ public:
         return p;
     }
 };
+
+//redo
+vector<int> getRow(int rowIndex) {
+  vector<int> ans(rowIndex+1);
+  ans[0] = 1;
+  for(int i = 1; i <= rowIndex; i++) {
+    ans[i] = 1;
+    for(int j = i-1; j > 0; j--) ans[j] += ans[j-1];
+  }
+  return ans;
+}
+
