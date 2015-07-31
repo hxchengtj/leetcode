01b3f7ac1fea63df461630d89c82bb0989b7d479
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-31 11:50:56 +0800

    Unique Binary Search Trees

--- Unique Binary Search Trees.cpp
+++ Unique Binary Search Trees.cpp
@@ -12,3 +12,18 @@ public:
         return f[n];
     }
 };
+
+
+//redo
+int numTrees(int n) {
+  if(n <= 0) return 0;
+  int f[n+1];
+  f[0] = 1;
+  for(int i = 1; i <= n; i++) {
+    f[i] = 0;
+    for(int j = 0; j < i; j++)
+      f[i] += f[j]*f[i-j-1];
+  }
+  return f[n];
+}
+
