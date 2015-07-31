d0a75f945f5e2dd06ab752e62c61e0fa53aade00
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-31 16:31:56 +0800

    Merge k Sorted Lists

--- Merge k Sorted Lists.cpp
+++ Merge k Sorted Lists.cpp
@@ -6,7 +6,7 @@ public:
         int i;
         vector<ListNode *> v;
         auto cmp = [](const ListNode *x, const ListNode *y) { return x->val > y->val;}; // ??
-        for(ListNode* l:lists) 
+        for(ListNode* l:lists)
             if(l)
                 v.push_back(l);
         make_heap(v.begin(), v.end(), cmp);
@@ -25,3 +25,34 @@ public:
         return HEAD.next;
     }
 };
+
+//redo
+ListNode* mergeKLists(vector<ListNode*>& lists) {
+  int k = lists.size();
+  ListNode* heap[k];
+  int heapsize = 0;
+  auto mycmp = [](const ListNode* a, const ListNode* b) {
+    return a->val > b->val;
+  };
+  for(auto l:lists) {
+    if(l) {
+      heap[heapsize++] = l;
+      push_heap(heap, heap+heapsize, mycmp);
+    }
+  }
+  ListNode HEAD(0);
+  ListNode* tail = &HEAD;
+  while(heapsize) {
+    ListNode *t = heap[0];
+    pop_heap(heap, heap+heapsize--, mycmp);
+    tail->next = t;
+    tail = tail->next;
+
+    if(t->next) {
+      heap[heapsize++] = t->next;
+      push_heap(heap, heap+heapsize, mycmp);
+    }
+  }
+  return HEAD.next;
+}
+
