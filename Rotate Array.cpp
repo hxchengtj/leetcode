9ae63640f5ef447b1f8568e9b680b6384b141e5d
ziyeqinghan <ziyeqinghan@gmail.com>
2015-07-31 14:05:52 +0800

    Rotate Array

--- Rotate Array.cpp
+++ Rotate Array.cpp
@@ -4,7 +4,7 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k%n;
-        for(int i = 0; i < n/2; i++) 
+        for(int i = 0; i < n/2; i++)
             swap(nums[i], nums[n-1-i]);
         for(int i = 0;i < k/2; i++)
             swap(nums[i], nums[k-1-i]);
@@ -12,3 +12,14 @@ public:
             swap(nums[i], nums[n+k-1-i]);
     }
 };
+
+//redo
+void RotateArray(vector<int>& nums, int k) {
+  int n = nums.size();
+  if(n == 0) return;
+  k %= n;
+  reverse(nums.begin(), nums.end());
+  reverse(nums.begin(), nums.begin()+k);
+  reverse(nums.begin()+k, nums.end());
+}
+
