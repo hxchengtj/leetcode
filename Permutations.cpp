a21cddb9d110e932c8b1743064ee5e514e6e9ef4
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-02 16:35:14 +0800

    Permutations

--- Permutations.cpp
+++ Permutations.cpp
@@ -27,3 +27,30 @@ public:
         }
     }
 };
+
+//redo
+vector<vector<int>> ans;
+vector<int> v;
+void __permute(vector<int>& nums, vector<bool>& visited, int n) {
+  if(n == 0) {
+    ans.push_back(v);
+    return;
+  }
+  for(int i = 0; i < nums.size(); i++) {
+    if(!visited[i]) {
+      visited[i] = true;
+      v.push_back(nums[i]);
+      __permute(nums, visited, n-1);
+      v.pop_back();
+      visited[i] = false;
+    }
+  }
+}
+vector<vector<int>> permute(vector<int>& nums) {
+  ans.clear(); v.clear();
+  int n = nums.size();
+  vector<bool> visited(n, false);
+  __permute(nums, visited, n);
+  return ans;
+}
+
