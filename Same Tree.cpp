aa443d4efb35d2d2a7bc90fa00322e821050a358
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-04 10:43:23 +0800

    Same Tree

--- Same Tree.cpp
+++ Same Tree.cpp
@@ -19,3 +19,12 @@ public:
         return true;
     }
 };
+
+//redo
+bool isSameTree(TreeNode* p, TreeNode* q) {
+  if(p == NULL && q == NULL) return true;
+  if((p == NULL && q != NULL) || (p != NULL && q == NULL)) return false;
+  if(p->val != q->val) return false;
+  return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
+
