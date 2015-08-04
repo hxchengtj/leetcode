9b02e9e086744b20e411801d4a984c7f6f65aaf4
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-04 11:40:48 +0800

    Excel Sheet Column Title

--- Excel Sheet Column Title.cpp
+++ Excel Sheet Column Title.cpp
@@ -12,3 +12,15 @@ public:
         return s;
     }
 };
+
+//redo
+string convertToTitle(int n) {
+  string ans;
+  while(n) {
+    ans.push_back((n-1)%26+'A');
+    n = (n-1)/26;
+  }
+  reverse(ans.begin(), ans.end());
+  return ans;
+}
+
