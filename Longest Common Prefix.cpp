a43641ee1a9315ee7f75baf0aa7be6ed8866ace0
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-04 18:04:01 +0800

    Longest Common Prefix

--- Longest Common Prefix.cpp
+++ Longest Common Prefix.cpp
@@ -6,12 +6,24 @@ public:
             return ans;
         ans = strs[0];
         int i, j;
-        for(i = 1; i < strs.size(); i++) { 
-            for(j = 0; j < ans.size() && j < strs[i].size() && ans[j] == strs[i][j]; j++) 
+        for(i = 1; i < strs.size(); i++) {
+            for(j = 0; j < ans.size() && j < strs[i].size() && ans[j] == strs[i][j]; j++)
                 ;
             ans.resize(j);
         }
         return ans;
     }
-    
+
 };
+
+//redo
+string longestCommonPrefix(vector<string> &strs) {
+  if(strs.size() == 0) return "";
+  int i, j;
+  for(i = 0; i < strs[0].size(); i++)
+    for(j = 1; j < strs.size(); j++) {
+      if(i >= strs[j].size() || strs[0][i] != strs[j][i]) return strs[0].substr(0, i);
+    }
+  return strs[0];
+}
+
