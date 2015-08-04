9be860cd9308401544358ecbc65a523cccd40c86
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-04 16:38:27 +0800

    Remove Duplicates from Sorted List

--- Remove Duplicates from Sorted List.cpp
+++ Remove Duplicates from Sorted List.cpp
@@ -6,12 +6,28 @@ public:
         int a = head->val;
         ListNode *h = head, *l = head->next;
         while(l) {
-            if(l->val == h->val) 
+            if(l->val == h->val)
                 h->next = l->next;
-            else 
+            else
                 h = l;
             l = l->next;
         }
         return head;
     }
 };
+
+//redo
+ListNode* deleteDuplicates(ListNode* head) {
+  if(head == NULL) return NULL;
+  ListNode* p = head;
+  while(p->next) {
+    ListNode* t = p->next;
+    if(p->val == t->val) {
+      p->next = t->next;
+      delete t;
+    }
+    else p = p->next;
+  }
+  return head;
+}
+
