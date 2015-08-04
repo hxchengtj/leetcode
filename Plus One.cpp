9c2870c442de7730717270c6b5aea4095722d696
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-04 17:02:23 +0800

    Plus One

--- Plus One.cpp
+++ Plus One.cpp
@@ -12,3 +12,18 @@ public:
         return digits;
     }
 };
+
+//redo
+vector<int> plusOne(vector<int>& digits) {
+  vector<int> ans;
+  int r = 1;
+  for(int i = digits.size()-1; i >= 0; i--) {
+    int a = digits[i]+r;
+    ans.push_back(a%10);
+    r = a/10;
+  }
+  if(r) ans.push_back(r);
+  reverse(ans.begin(), ans.end());
+  return ans;
+}
+
