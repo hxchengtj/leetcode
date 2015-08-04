e0c40e4bb50905e8adadd2e8d3d93a6707fcdcb7
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-04 16:12:55 +0800

    Palindrome Number

--- Palindrome Number.cpp
+++ Palindrome Number.cpp
@@ -18,3 +18,17 @@ public:
         return true;
     }
 };
+
+//redo
+bool isPalindrome(int x) {
+  if(x < 0) return false;
+  int i = 1, j = 1;
+  while(x/i >= 10) i *= 10;
+  while(i > j) {
+    if((x/i)%10 != (x/j)%10) return false;
+    i /= 10;
+    j *= 10;
+  }
+  return true;
+}
+
