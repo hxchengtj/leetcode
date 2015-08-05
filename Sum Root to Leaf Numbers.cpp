00a73d79f3b954ba13631980e92b53519cc1dfb6
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-05 10:57:38 +0800

    Sum Root to Leaf Numbers

---
+++ Sum Root to Leaf Numbers.cpp
@@ -0,0 +1,15 @@
+int sumNumbers(TreeNode* root) {
+  if(root == NULL) return 0;
+  return __sumNumbers(root, 0);
+}
+
+int __sumNumbers(TreeNode* root, int a) {
+  a = a*10+root->val;
+  if(root->left == NULL && root->right == NULL) return a;
+  int ans = 0;
+  if(root->left) ans += __sumNumbers(root->left, a);
+  if(root->right) ans += __sumNumbers(root->right, a);
+  return ans;
+}
+
+
