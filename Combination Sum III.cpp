dc79f6ee44e812eefc70276169f5716612303170
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-05 18:04:19 +0800

    Combination Sum III

--- Combination Sum III.cpp
+++ Combination Sum III.cpp
@@ -25,3 +25,30 @@ public:
     }
 };
 
+
+//redo
+vector<vector<int>> ans;
+vector<int> v;
+vector<vector<int>> combinationSum3(int k, int n) {
+  ans.clear(); v.clear();
+  if(k == 0) return ans;
+  dfs(1, k, n);
+  return ans;
+}
+
+void dfs(int a, int k, int n) {
+  if(k == 1) {
+    if(n >= a && n <= 9) {
+      v.push_back(n);
+      ans.push_back(v);
+      v.pop_back();
+    }
+    return;
+  }
+  for(int i = a; i <= min(n, 9); i++) {
+    v.push_back(i);
+    dfs(i+1, k-1, n-i);
+    v.pop_back();
+  }
+}
+
