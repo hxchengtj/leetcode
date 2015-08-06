be4434d5c641eb5104260c7d0f72940f2078a422
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-06 17:21:01 +0800

    Maximal Rectangle

--- Maximal Rectangle.cpp
+++ Maximal Rectangle.cpp
@@ -74,3 +74,38 @@ int main() {
     return 0;
 }
 
+//redo
+int __maximalRectangle(vector<int>& sum) {
+  int n = sum.size();
+  stack<int> stk;
+  int left[n];
+  for(int i = 0; i < n; i++) {
+    while(!stk.empty() && sum[stk.top()] >= sum[i]) stk.pop();
+    left[i] = stk.empty()? 0:stk.top()+1;
+    stk.push(i);
+  }
+  stk = stack<int>();
+  int ans = 0, right;
+  for(int i = n-1; i >= 0; i--) {
+    while(!stk.empty() && sum[stk.top()] >= sum[i]) stk.pop();
+    right = stk.empty()? n:stk.top();
+    ans = max(ans, (right-left[i])*sum[i]);
+    stk.push(i);
+  }
+  return ans;
+}
+int maximalRectangle(vector<vector<char>>& matrix) {
+  if(matrix.size() == 0 || matrix[0].size() == 0) return 0;
+  int m = matrix.size(), n = matrix[0].size();
+  int ans = 0;
+  vector<int> sum(n, 0);
+  for(int i = 0; i < m; i++) {
+    for(int j = 0; j < n; j++) {
+      if(matrix[i][j] == '1') sum[j] += 1;
+      else sum[j] = 0;
+    }
+    ans = max(ans, __maximalRectangle(sum));
+  }
+  return ans;
+}
+
