f76fdf70befe58f7f90dd972bdc6afa48deceefa
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-06 15:09:38 +0800

    Word Ladder II

--- Word Ladder II.cpp
+++ Word Ladder II.cpp
@@ -16,7 +16,7 @@ public:
         g.push_back(0);
         x = 0;
         while(l < q.size()) {
-            if(l > g.back()) 
+            if(l > g.back())
                 g.push_back(q.size()-1);
             s = q[l];
             if(s == end) {
@@ -73,3 +73,55 @@ public:
         return b;
     }
 };
+
+//redo
+vector<vector<string>> ans;
+vector<string> v;
+vector<vector<string>> findLadders(string start, string end, unordered_set<string> &dict) {
+  unordered_map<string, int> m;
+  queue<string> q;
+  q.push(start);
+  m[start] = 1;
+  int curcount = 0;
+  while(!q.empty()) {
+    string t = q.front();
+    q.pop();
+    curcount = m[t];
+    if(t == end) break;
+    for(int i = 0; i < t.size(); i++) {
+      char c = t[i];
+      for(t[i] = 'a'; t[i] <= 'z'; t[i]++) {
+        if((t == end || dict.find(t) != dict.end()) && m.find(t) == m.end()) {
+          m[t] = curcount+1;
+          q.push(t);
+        }
+      }
+      t[i] = c;
+    }
+  }
+  if(m.find(end) == m.end()) return {};
+  ans.clear(); v.clear();
+  __findLadders(end, m[end], m);
+  return ans;
+}
+
+void __findLadders(string end, int curcount, unordered_map<string, int>& m) {
+  if(curcount == 1) {
+    v.push_back(end);
+    reverse(v.begin(), v.end());
+    ans.push_back(v);
+    reverse(v.begin(), v.end());
+    v.pop_back();
+    return;
+  }
+  v.push_back(end);
+  for(int i = 0; i < end.size(); i++) {
+    char c = end[i];
+    for(end[i] = 'a'; end[i] <= 'z'; end[i]++)
+      if(m.find(end) != m.end() && m[end] == curcount-1)
+        __findLadders(end, curcount-1, m);
+    end[i] = c;
+  }
+  v.pop_back();
+}
+
