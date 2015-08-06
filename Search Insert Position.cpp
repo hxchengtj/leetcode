86f213b3430200959b29b9cd39521d782e719ab0
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-06 20:09:02 +0800

    Search Insert Position

--- Search Insert Position.cpp
+++ Search Insert Position.cpp
@@ -6,9 +6,22 @@ public:
             m = (l+r)/2;
             if(A[m] < target)
                 l = m+1;
-            else 
+            else
                 r = m-1;
         }
         return l;
     }
 };
+
+//redo
+int searchInsert(vector<int>& nums, int target) {
+  int l = 0, r = nums.size();
+  while(l < r) {
+    int m = l + (r-l)/2;
+    if(nums[m] < target) l = m+1;
+    else r = m;
+  }
+  return l;
+}
+
+
