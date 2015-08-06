5a636fe08f713d1a022cd473d26528bfe324b329
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-06 12:05:20 +0800

    Single Number

--- Single Number.cpp
+++ Single Number.cpp
@@ -6,3 +6,12 @@ public:
         return A[0];
     }
 };
+
+//redo
+int singleNumber(vector<int>& nums) {
+  int ans = nums[0];
+  for(int i = 1; i < nums.size(); i++)
+    ans ^= nums[i];
+  return ans;
+}
+
