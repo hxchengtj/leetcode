b48c0b90bcac00b14286168913967cf2b57713ae
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-08 15:48:39 +0800

    3Sum

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -13,7 +13,7 @@ public:
             k = n-1;
             j = i+1;
             while(j < k) { 					// two pointers
-                t = num[i]+num[j]+num[k];	
+                t = num[i]+num[j]+num[k];
                 if(t > 0)
                     k--;
                 else if (t < 0)
@@ -27,7 +27,7 @@ public:
                     j++;
                     k--;
                 }
-            } 
+            }
         }
         return ans;
     }
@@ -71,3 +71,26 @@ public:
     }
 };
 };
+
+//redo
+vector<vector<int>> threeSum(vector<int>& nums) {
+  sort(nums.begin(), nums.end());
+  vector<vector<int>> ans;
+  int n = nums.size();
+  for(int i = 0; i < n-2; i++) {
+    if(i > 0 && nums[i] == nums[i-1]) continue;
+    int j = i+1, k = n-1;
+    while(j < k) {
+      if(j > i+1 && nums[j] == nums[j-1]) j++;
+      else if(k < n-1 && nums[k] == nums[k+1]) k--;
+      else if(nums[i]+nums[j]+nums[k] < 0) j++;
+      else if(nums[i]+nums[j]+nums[k] > 0) k--;
+      else {
+        ans.push_back({nums[i], nums[j], nums[k]});
+        j++; k--;
+      }
+    }
+  }
+  return ans;
+}
+
