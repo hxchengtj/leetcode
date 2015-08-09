cfd90ce0a6b447f6700e88bceca8831e1f091d99
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-09 17:28:11 +0800

    Sliding Window Maximum

--- Sliding Window Maximum.cpp
+++ Sliding Window Maximum.cpp
@@ -14,3 +14,20 @@ public:
         return ans;
     }
 };
+
+//redo
+vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+  if(k <= 0) return {};
+  deque<int> d;
+  vector<int> ans;
+  for(int i = 0; i < nums.size(); i++) {
+    while(!d.empty() && nums[d.back()] < nums[i]) d.pop_back();
+    d.push_back(i);
+    if(i >= k-1) {
+      while(d.front() <= i-k) d.pop_front();
+      ans.push_back(nums[d.front()]);
+    }
+  }
+  return ans;
+}
+
