60395838d2357daa73c5665dbfd8c3d7cd8ca3a2
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-09 18:04:30 +0800

    Valid Sudoku

--- Valid Sudoku.cpp
+++ Valid Sudoku.cpp
@@ -22,3 +22,17 @@ public:
         return true;
     }
 };
+
+//redo
+bool isValidSudoku(vector<vector<char>>& board) {
+  vector<vector<bool>> row(9, vector<bool>(9, false)), column(9, vector<bool>(9, false)), block(9, vector<bool>(9, false));
+  for(int i = 0; i < board.size(); i++)
+    for(int j = 0; j < board[0].size(); j++) {
+      if(board[i][j] == '.') continue;
+      int a = board[i][j]-'1';
+      if(row[i][a] || column[j][a] || block[(i/3)*3+j/3][a]) return false;
+      row[i][a] = column[j][a] = block[(i/3)*3+j/3][a] = true;
+    }
+  return true;
+}
+
