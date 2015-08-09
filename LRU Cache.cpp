c97f64c59415d4cee2a7232e2588ca87bcf9c1e6
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-09 20:10:20 +0800

    LRU Cache

--- LRU Cache.cpp
+++ LRU Cache.cpp
@@ -7,7 +7,7 @@ public:
         n = capacity;
         i = 0;
     }
-    
+
     int get(int key) {
         auto iter = m.find(key);
         if(iter != m.end()) {
@@ -19,7 +19,7 @@ public:
         }
         return -1;
     }
-    
+
     void set(int key, int value) {
         auto iter = m.find(key);
         if(iter != m.end()) {
@@ -31,7 +31,7 @@ public:
         }
         else {
             if(i < n) i++;
-            else { 
+            else {
                 int t = l.begin()->first;
                 m.erase(t);
                 l.pop_front();
@@ -42,3 +42,38 @@ public:
         }
     }
 };
+
+//redo
+class LRUCache {
+  public:
+    unordered_map<int, list<pair<int, int>>::iterator> m;
+    list<pair<int, int>> l;
+    int maxn, n = 0;
+    LRUCache(int capacity) {
+      maxn = capacity;
+    }
+
+    int get(int key) {
+      auto iter = m.find(key);
+      if(iter == m.end()) return -1;
+      l.push_back(*(iter->second));
+      l.erase(iter->second);
+      m[key] = prev(l.end());
+      return l.back().second;
+    }
+
+    void set(int key, int value) {
+      auto iter = m.find(key);
+      if(iter == m.end()) n++;
+      else l.erase(iter->second);
+
+      if(n > maxn) {
+        m.erase(l.front().first);
+        l.pop_front();
+        n--;
+      }
+      l.push_back(make_pair(key, value));
+      m[key] = prev(l.end());
+    }
+};
+
