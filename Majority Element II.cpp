af3f2cbab481b214b6bcf236716aa97e88b77e3b
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-09 18:45:12 +0800

    Majority Element II

--- Majority Element II.cpp
+++ Majority Element II.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         int a = 0, an = 0, b = 1, bn = 0;
         for(int i = 0; i < nums.size(); i++) {
-            if(a == nums[i]) 
+            if(a == nums[i])
                 an++;
             else if(b == nums[i])
                 bn++;
@@ -36,3 +36,32 @@ public:
     }
 
 };
+
+//redo
+vector<int> majorityElement(vector<int>& nums) {
+  int num1 = 0, num2 = -1, count1 = 0, count2 = 0;
+  for(auto a:nums) {
+    if(num1 == a) count1++;
+    else if(num2 == a) count2++;
+    else if(count1 == 0) {
+      num1 = a;
+      count1++;
+    } else if(count2 == 0) {
+      num2 = a;
+      count2++;
+    } else {
+      count1--;
+      count2--;
+    }
+  }
+  count1 = 0; count2 = 0;
+  for(auto a:nums) {
+    if(a == num1) count1++;
+    else if(a == num2) count2++;
+  }
+  vector<int> ans;
+  if(count1 > nums.size()/3) ans.push_back(num1);
+  if(count2 > nums.size()/3) ans.push_back(num2);
+  return ans;
+}
+
