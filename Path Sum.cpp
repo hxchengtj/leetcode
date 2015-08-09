8d79e7e37903fd10e8e1e442da8da8b2c8e78575
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-09 18:12:16 +0800

    Path Sum

--- Path Sum.cpp
+++ Path Sum.cpp
@@ -23,3 +23,12 @@ public:
         return hasPathSum(root->left, sum) || hasPathSum(root->right, sum);
     }
 };
+
+//redo
+bool hasPathSum(TreeNode* root, int sum) {
+  if(root == NULL) return false;
+  if(root->left == NULL && root->right == NULL && root->val == sum)
+    return true;
+  return hasPathSum(root->left, sum-root->val) || hasPathSum(root->right, sum-root->val);
+}
+
