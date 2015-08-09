d7e272fa3b0ba6e2aef9a33a9255b5f0abc45635
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-09 20:32:16 +0800

    4Sum

--- 4Sum.cpp
+++ 4Sum.cpp
@@ -35,3 +35,31 @@ public:
         return ans;
     }
 };
+
+//redo
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+  sort(nums.begin(), nums.end());
+  int n = nums.size();
+  vector<vector<int>> ans;
+  for(int i = 0;i < n-3.; i++) {
+    if(i > 0 && nums[i] == nums[i-1]) continue;
+    for(int j = i+1; j < n-2; j++) {
+      if(j > i+1 && nums[j] == nums[j-1]) continue;
+      int k = j+1, l = n-1;
+      while(k < l) {
+        int sum = nums[i]+nums[j]+nums[k]+nums[l];
+        if(k > j+1 && nums[k] == nums[k-1]) k++;
+        else if(l < n-1 && nums[l] == nums[l+1]) l--;
+        else if(sum < target) k++;
+        else if(sum > target) l--;
+        else {
+          ans.push_back({nums[i],nums[j],nums[k],nums[l]});
+          k++;
+          l--;
+        }
+      }
+    }
+  }
+  return ans;
+}
+
