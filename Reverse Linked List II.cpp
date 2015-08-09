b65745874666dd890854d2d11110a4ce72f45659
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-09 12:06:56 +0800

    Reverse Linked List II

--- Reverse Linked List II.cpp
+++ Reverse Linked List II.cpp
@@ -5,7 +5,7 @@ public:
     ListNode *reverseBetween(ListNode *head, int m, int n) {
         ListNode *h = head, *h1 = head, *p1 = NULL, *lm, *h2;
         int i = 1, j = 0;
-        for(i = 1; i < m; h = h->next, i++) 
+        for(i = 1; i < m; h = h->next, i++)
             if(i == m-1)
                 p1 = h;
         lm = h;
@@ -24,3 +24,21 @@ public:
         return head;
     }
 };
+
+//redo
+ListNode* reverseBetween(ListNode* head, int m, int n) {
+  ListNode HEAD(0);
+  HEAD.next = head;
+  ListNode* prev = &HEAD;
+  for(int i = 1; i < m; i++) prev = prev->next;
+  ListNode *tail = prev->next, *p = tail->next;
+  for(int i = m; i < n; i++) {
+    ListNode* t = p->next;
+    p->next = prev->next;
+    prev->next = p;
+    tail->next = t;
+    p = t;
+  }
+  return HEAD.next;
+}
+
