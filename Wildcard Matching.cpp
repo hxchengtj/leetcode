eee5b7ea90df9b417b686de1509309d3b9d8f9d2
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-10 15:31:29 +0800

    Wildcard Matching

--- Wildcard Matching.cpp
+++ Wildcard Matching.cpp
@@ -21,21 +21,21 @@ public:
         f[0] = true;
         for(i = 1; i <= n; i++) {
             if(v[i-1][0] == '*') {
-                for(j = 1; j <= m; j++) 
+                for(j = 1; j <= m; j++)
                     f[j] = f[j-1] || f[j];
             }
             else {
-                k = v[i-1].length(); 
+                k = v[i-1].length();
                 for(j = m; j >= 0; j--) {
                     if(j >= k)
-                        f[j] = f[j-k] && mystrcmp(v[i-1], s, j-k); 
+                        f[j] = f[j-k] && mystrcmp(v[i-1], s, j-k);
                     else
                         f[j] = false;
                 }
             }
         }
         return f[m];
-        
+
     }
     bool mystrcmp(string x, const char *s, int a) {
         for(int i = 0; i < x.size(); i++)
@@ -68,7 +68,7 @@ public:
                 while(bp < i && p[bp] == '*') bp++;
                 if(bp < i) {
                     string::iterator it;
-                    if(i == np) 
+                    if(i == np)
                         it = search(s.begin() + max(bs, ns-i+bp), s.end(), p.begin()+bp, p.begin()+i, equalmatch);
                     else
                         it = search(s.begin()+bs, s.end(), p.begin()+bp, p.begin()+i, equalmatch);
@@ -81,3 +81,30 @@ public:
         return true;
     }
 };
+
+//redo
+bool isMatch(string s, string p) {
+  int ns = s.size(), np = p.size();
+  if(np == 0) {
+    if(ns == 0) return true;
+    else return false;
+  }
+  auto cmp = [](const char & a, const char & b){
+    return a == b || a == '?' || b == '?';
+  };
+  int si = 0, pi = 0;
+  for(int i = 0; i <= np; i++) {
+    if(i == np || p[i] == '*') {
+      while(pi < i && p[pi] == '*') pi++;
+      if(pi != i) {
+        if(i == np) si = max(si, ns-np+pi);
+        auto iter = search(s.begin()+si, s.end(), p.begin()+pi, p.begin()+i, cmp);
+        if(iter == s.end() || (pi == 0 && iter != s.begin())) return false;
+        si = distance(s.begin(), iter)+i-pi;
+      }
+      pi = i+1;
+    }
+  }
+  return true;
+}
+
