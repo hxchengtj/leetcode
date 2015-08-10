12094b14327ae118be2d1e6d31fe6366e6691e34
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-10 11:10:22 +0800

    Sqrt(x)

--- Sqrt(x).cpp
+++ Sqrt(x).cpp
@@ -16,3 +16,16 @@ public:
         return r;             //舍掉小数位
     }
 };
+
+//redo
+int mySqrt(int x) {
+  if(x < 0) return -1;
+  if(x == 0) return x;
+  int l = 1, r = x;
+  while(l <= r) {
+    int m = l+(r-l)/2;
+    if(m > x/m) r = m-1;
+    else l = m+1;
+  }
+  return r;
+}
