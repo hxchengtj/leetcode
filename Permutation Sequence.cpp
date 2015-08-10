8cc213611aa1130984bd85ab3546c97f837bb606
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-10 19:26:13 +0800

    Permutation Sequence

--- Permutation Sequence.cpp
+++ Permutation Sequence.cpp
@@ -25,3 +25,28 @@ public:
         return s;
     }
 };
+
+//redo
+string getPermutation(int n, int k) {
+  int t = 1;
+  vector<bool> visited(n, false);
+  string ans;
+  for(int i = 2; i < n; i++)
+    t *= i;
+  k--;
+  for(int i = n-1; i >= 0; i--) {
+    int j = k/t+1, l = 0;
+    for(int a = 0; a < n; a++) {
+      if(visited[a] == false) l++;
+      if(j == l) {
+        visited[a] = true;
+        ans.push_back(a+'1');
+        break;
+      }
+    }
+    k = k%t;
+    if(i > 0) t /= i;
+  }
+  return ans;
+}
+
