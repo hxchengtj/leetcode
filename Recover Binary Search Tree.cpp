9b35351911f7b551d113ae4052a5c1d1011ff9c6
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-11 09:59:18 +0800

    Recover Binary Search Tree

--- Recover Binary Search Tree.cpp
+++ Recover Binary Search Tree.cpp
@@ -17,6 +17,28 @@ public:
         }
         a = root->val;
         p = root;
-        dfs(root->right);   
+        dfs(root->right);
     }
 };
+
+//redo
+void recoverTree(TreeNode* root) {
+  TreeNode* p = root, * pre = NULL, *a = NULL, *b = NULL;
+  stack<TreeNode*> stk;
+  while(p || !stk.empty()) {
+    while(p) {
+      stk.push(p);
+      p = p->left;
+    }
+    TreeNode* t = stk.top();
+    stk.pop();
+    if(pre != NULL && pre->val > t->val) {
+      if(a == NULL) a = pre;
+      b = t;
+    }
+    pre = t;
+    p = t->right;
+  }
+  swap(a->val, b->val);
+}
+
