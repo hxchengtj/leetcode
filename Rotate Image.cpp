f8d61588540ab0a1b1c4c4ffa007834afdb7904d
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-11 10:33:31 +0800

    Rotate Image

--- Rotate Image.cpp
+++ Rotate Image.cpp
@@ -21,10 +21,24 @@ public:
     void rotate(vector<vector<int> > &matrix) {
         int i, j, n = matrix.size(), t;
         for(i = 0; i < n; i++)
-            for(j = 0; j < i; j++)   
+            for(j = 0; j < i; j++)
                 swap(matrix[i][j], matrix[j][i]);
         for(i = 0; i < n; i++)
             for(j = 0; j < n/2; j++)
                 swap(matrix[i][j], matrix[i][n-1-j]);
     }
 };
+
+//redo
+void rotate(vector<vector<int>>& matrix) {
+  int n = matrix.size();
+  for(int i = 0; i < n/2; i++)
+    for(int j = 0; j < (n+1)/2; j++) {
+      int a = matrix[i][j];
+      matrix[i][j] = matrix[n-1-j][i];
+      matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+      matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+      matrix[j][n-1-i] = a;
+    }
+}
+
