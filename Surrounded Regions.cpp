7e896a41f525b89adc217fba604fbeb2319ce80d
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-11 16:17:29 +0800

    Surrounded Regions

--- Surrounded Regions.cpp
+++ Surrounded Regions.cpp
@@ -53,4 +53,42 @@ public:
 };
 
 //redo
+void dfs(vector<vector<char>>& board, int x, int y) {
+  queue<pair<int, int>> q;
+  if(board[x][y] == 'O') {
+    q.push(make_pair(x, y));
+    board[x][y] = 'A';
+    while(!q.empty()) {
+      pair<int, int> p = q.front();
+      q.pop();
+      int f[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+      for(int i = 0; i < 4; i++) {
+        int x1 = p.first+f[i][0], y1 = p.second+f[i][1];
+        if(x1 >= 0 && x1 < board.size() && y1 >= 0 && y1 < board[0].size() && board[x1][y1] == 'O') {
+          board[x1][y1] = 'A';
+          q.push(make_pair(x1, y1));
+        }
+      }
+    }
+  }
+}
+
+void solve(vector<vector<char>>& board) {
+  if(board.size() == 0 || board[0].size() == 0) return;
+  int m = board.size(), n = board[0].size();
+  for(int i = 0; i < m; i++) {
+    dfs(board, i, 0);
+    dfs(board, i, n-1);
+  }
+  for(int i = 0; i < n; i++) {
+    dfs(board, 0, i);
+    dfs(board, m-1, i);
+  }
+  for(int i = 0; i < m; i++)
+    for(int j = 0; j < n; j++)
+      if(board[i][j] == 'O') board[i][j] = 'X';
+      else if(board[i][j] == 'A') board[i][j] = 'O';
+}
+
+
 
