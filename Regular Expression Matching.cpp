8f5e0b2464c76e8ca0d10e05458b8543f4d5d16f
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-11 16:02:12 +0800

    Regular Expression Matching

--- Regular Expression Matching.cpp
+++ Regular Expression Matching.cpp
@@ -47,3 +47,17 @@ public:
         return true;
     }
 };
+
+//redo
+bool isMatch(string s, string p) {
+  int m = s.size(), n = p.size();
+  vector<vector<bool>> f(m+1, vector<bool>(n+1, false));
+  f[0][0] = true;
+  for(int i = 0; i <= m; i++)
+    for(int j = 1; j <= n; j++)
+      if(p[j-1] == '*') f[i][j] = f[i][j-2] || (i > 0 && f[i-1][j] && (s[i-1] == p[j-2] || p[j-2] == '.'));
+      else f[i][j] = i > 0 && f[i-1][j-1] && (s[i-1] == p[j-1] || p[j-1] == '.');
+  return f[m][n];
+}
+
+
