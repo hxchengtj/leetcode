0166fbcaf5a81472922c7d99c841fc55c00a08c3
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-12 14:41:58 +0800

    Search a 2D Matrix II

---
+++ Search a 2D Matrix II.cpp
@@ -0,0 +1,27 @@
+
+ol searchMatrix(vector<vector<int>>& matrix, int target) {
+  if(matrix.size() == 0 || matrix[0].size() == 0) return false;
+  int m = matrix.size(), n = matrix[0].size();
+  int xl = 0, xr = m, yl = 0, yr = n;
+  while(xl < xr && yl < yr) {
+    int l = xl, r = xr;
+    while(l < r) {
+      int mid = l+(r-l)/2;
+      if(matrix[mid][yl] == target) return true;
+      else if(matrix[mid][yl] < target) l = mid+1;
+      else r = mid;
+    }
+    xr = l;
+    l = yl; r = yr;
+    while(l < r) {
+      int mid = l+(r-l)/2;
+      if(matrix[xl][mid] == target) return true;
+      else if(matrix[xl][mid] < target) l = mid+1;
+      else r = mid;
+    }
+    yr = l;
+    xl++; yl++;
+  }
+  return false;
+}
+
