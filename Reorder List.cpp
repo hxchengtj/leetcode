192374cdbb68317c5476eeb828542367c546a60a
ziyeqinghan <ziyeqinghan@gmail.com>
2015-08-13 19:31:40 +0800

    Reorder List

--- Reorder List.cpp
+++ Reorder List.cpp
@@ -3,7 +3,7 @@ public:
     void reorderList(ListNode *head) {
         ListNode *l = head, *p, *t, *t1;
         int i, n = 0;
-        for(n = 0; l != NULL; n++) 
+        for(n = 0; l != NULL; n++)
             l = l->next;
         l = head;
         for(i = 0; i < n/2; i++)
@@ -28,3 +28,43 @@ public:
             l->next = NULL;
     }
 };
+
+//redo
+ListNode* reverse_list(ListNode* head) {
+  if(head == NULL) return head;
+  ListNode* p = head, *np = head->next;
+  head->next = NULL;
+  while(np) {
+    ListNode* t = np->next;
+    np->next = p;
+    p = np;
+    np = t;
+  }
+  return p;
+}
+void reorderList(ListNode* head) {
+  if(head == NULL || head->next == NULL) return ;
+  ListNode* p = head, *q = head->next;
+  while(q) {
+    q = q->next;
+    if(q) {
+      q = q->next;
+      p = p->next;
+    }
+  }
+  ListNode* head1 = reverse_list(p->next);
+  p->next = NULL;
+  ListNode HEAD(0);
+  ListNode* tail = &HEAD;
+  while(head && head1) {
+    tail->next = head;
+    head = head->next;
+    tail = tail->next;
+    tail->next = head1;
+    head1 = head1->next;
+    tail = tail->next;
+  }
+  if(head) tail->next = head;
+  if(head1) tail->next = head1;
+}
+
