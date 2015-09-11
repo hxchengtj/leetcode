af3b4de5527f3ebdd7079f0f5c7b9e8ff8709824
ziyeqinghan <ziyeqinghan@gmail.com>
2015-09-11 18:44:55 +0800

    H-Index

---
+++ H-Index.cpp
@@ -0,0 +1,13 @@
+int hIndex(vector<int>& citations) {
+  int n = citations.size();
+  int count[n+1];
+  memset(count, 0, sizeof(count));
+  for(auto c:citations) count[min(c, n)]++;
+  int a = 0;
+  for(int i = n; i > 0; i--) {
+    a += count[i];
+    if(a >= i) return i;
+  }
+  return 0;
+}
+
