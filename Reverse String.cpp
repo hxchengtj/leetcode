60ceaeea66ac27ad5d57f01cb80c5ca70172f6d9
ziyeqinghan <ziyeqinghan@gmail.com>
2016-06-19 12:24:20 +0800

    Reverse String

---
+++ Reverse String.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string reverseString(string s) {
+        if(s.size() == 0) return s;
+        int i=0, j = s.size()-1;
+        while(i < j) {
+            swap(s[i], s[j]);
+            i++;
+            j--;
+        }
+        return s;
+    }
+};
