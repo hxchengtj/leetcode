65c9894bca576d193dfa7f46e26a7c363baa2d53
ziyeqinghan <ziyeqinghan@gmail.com>
2016-07-15 21:58:36 +0800

    Maximum Product of Word Lengths

---
+++ Maximum Product of Word Lengths.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+        int n = words.size();
+        if(n == 0) return 0;
+        vector<int> mask(n);
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            for(char c:words[i]) mask[i] |= (1 << (c-'a'));
+            for(int j = 0; j < i; j++) 
+                if (!(mask[i] & mask[j])) ans = max(ans, int(words[i].size()*words[j].size()));
+        }
+        return ans;
+        
+    }
+};
