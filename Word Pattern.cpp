bec633f65a362b0b888f3c4aa91a2db7ccc78ec9
ziyeqinghan <ziyeqinghan@gmail.com>
2016-07-17 16:17:18 +0800

    Word Pattern

---
+++ Word Pattern.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string str) {
+        vector<string> dict(26);
+        unordered_map<string, char> m;
+        int i = 0, n = str.size();
+        for(char c:pattern) {
+            while(i < n && str[i] == ' ') i++;
+            int j = i;
+            while(i < n && str[i] != ' ') i++;
+            if(j == i) return false;
+            string s = str.substr(j, i-j);
+            if(dict[c-'a'].size() > 0 && dict[c-'a'] != s ) return false;
+            if(m.find(s) != m.end() && m[s] != c) return false;
+            dict[c-'a'] = s;
+            m[s] = c;
+        }
+        while(i < n && str[i] == ' ') i++;
+        return i == n;
+    }
+};
