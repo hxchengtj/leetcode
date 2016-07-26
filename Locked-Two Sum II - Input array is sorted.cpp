e59be3e0e741f10c2581e7a012dedc36b8d92273
ziyeqinghan <ziyeqinghan@gmail.com>
2016-07-26 17:02:55 +0800

    *Lock* Two Sum II - Input array is sorted

---
+++ Locked-Two Sum II - Input array is sorted.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+  vector<int> twoSum(vector<int> &numbers, int target) {
+    int n = numbers.size();
+    if(n <= 1) return {};
+    int i = 0, j = n-1;
+    while(i < j) {
+      int sum = numbers[i] + numbers[j];
+      if(sum < target) i++;
+      else if(sum > target) j--;
+      else return {i+1, j+1};
+    }
+    return {};
+  }
+};
