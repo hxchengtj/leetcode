227d444101b3f74004b80a7fcc638aac210a34b1
ziyeqinghan <ziyeqinghan@gmail.com>
2016-07-28 11:10:45 +0800

    Combination Sum IV

---
+++ Combination Sum IV.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> f(target+1);
+        f[0] = 1;
+        for(int i = 1; i <= target; i++)
+            for(int num:nums) 
+                if(num <= i)
+                f[i] += f[i-num];
+        return f[target];
+    }
+};
