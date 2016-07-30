ce0865404f18729234f160b0797fc11fc84f8d42
ziyeqinghan <ziyeqinghan@gmail.com>
2016-07-30 16:32:09 +0800

    *Lock* Line Reflection

---
+++ Locked-Line Reflection.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+  bool isReflected(vector<pair<int, int>>& points) {
+    if(points.empty()) return true;
+    int minp = min_element(points.begin(), points.end())->first;
+    int maxp = max_element(points.begin(), points.end())->first;
+    unordered_map<int, unordered_set<int>> m;
+    for(auto& p:points) m[p.first].insert(p.second);
+    for(auto& p:points) {
+      int t = minp+maxp-p.first;
+      if(m.find(t) == m.end() || m[t].find(p.second) == m[t].end()) return false;
+    }
+    return true;
+  }
+};
