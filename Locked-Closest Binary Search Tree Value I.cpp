436e434e3cdd6c2c7997147ded66302a2463bd1a
ziyeqinghan <ziyeqinghan@gmail.com>
2016-08-02 16:22:32 +0800

    *Lock* Closest Binary Search Tree Value I *iterative*

--- Locked-Closest Binary Search Tree Value I.cpp
+++ Locked-Closest Binary Search Tree Value I.cpp
@@ -1,3 +1,4 @@
+//recursive
 class Solution {
 public:
   int closestValue(TreeNode* root, double target) {
@@ -6,5 +7,18 @@ public:
     if(!t) return root->val;
     int a = closestValue(t, target), b = root->val;
     return abs(a-target) < abs(b-target) ? a:b;
+  }
+};
+
+//iterative
+class Solution {
+public:
+  int closestValue(TreeNode* root, double target) {
+    int ans = root->val;
+    while(root) {
+      if(abs(ans-target) > abs(root->val-target)) ans = root->val;
+      root = root->val < target? root->right: root->left;
+    }
+    return ans;
   }
 };
