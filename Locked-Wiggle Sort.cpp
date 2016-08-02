c03a06d7a33ccd8fa479e29fe65b7e4b700bef1a
ziyeqinghan <ziyeqinghan@gmail.com>
2016-08-02 15:20:29 +0800

    *Lock* Wiggle Sort

---
+++ Locked-Wiggle Sort.cpp
@@ -0,0 +1,10 @@
+//redo
+class Solution {
+public:
+  void wiggleSort(vector<int>& nums) {
+    for(int i = 1; i < nums.size(); i++) {
+      if((i&1) == (nums[i-1] > nums[i]))
+        swap(nums[i-1], nums[i]);
+    }
+  }
+};
