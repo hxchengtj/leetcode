6eaae997bca0c2adde433a4c794de513189e5264
ziyeqinghan <ziyeqinghan@gmail.com>
2016-09-10 22:03:16 +0800

    redo Minimum Depth of Binary Tree

--- Minimum Depth of Binary Tree.cpp
+++ Minimum Depth of Binary Tree.cpp
@@ -1,3 +1,4 @@
+/* review again */
 class Solution {
 public:
     int minDepth(TreeNode *root) {
@@ -51,4 +52,13 @@ int minDepth(TreeNode* root) {
   return ans;
 }
 
-
+//version2
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root == NULL) return 0;
+        if(!root->left) return minDepth(root->right)+1;
+        if(!root->right) return minDepth(root->left)+1;
+        return min(minDepth(root->left), minDepth(root->right))+1;
+    }
+};
