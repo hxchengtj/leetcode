13be3bdbb8328f553ffa84ad8994bd0302ca4a45
ziyeqinghan <ziyeqinghan@gmail.com>
2016-09-18 22:29:49 +0800

    modify *Lock* One Edit Distance

--- Locked-One Edit Distance.cpp
+++ Locked-One Edit Distance.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-  bool isOneEditDistance(string s, string t) {
-    int n = s.size(), m = t.size();
-    if(abs(n-m) > 1) return false;
-    if(n > m) return isOneDelete(s, t);
-    else if(n < m) return isOneDelete(t, s);
-    return s == t;
-  }
-  bool isOneDelete(string a, string b) {
-    bool isDelete = false;
-    int i = 0, j = 0, m = a.size(), n = b.size();
-    while(j < n) {
-      if(a[i] != b[j]) {
-        if(isDelete) return false;
-        isDelete = true;
-        i++;
-      } else {
-        i++; j++;
-      }
+    bool isOneEditDistance(string s, string t) {
+        int m = s.size(), n = t.size();
+        if(abs(m-n) > 1) return false;
+        int i = 0, j = 0, cnt = 0;
+        while(i < m || j < n) {
+            if(i == m || j == n || s[i] != t[j]) {
+                cnt++;
+                if(m < n) j++;
+                else if(m > n) i++;
+                else {
+                    i++; j++;
+                }
+            }
+            else {
+                i++; j++;
+            }
+            if(cnt > 1) return false;
+        }
+        return cnt == 1;
     }
-    return true;
-  }
 };
