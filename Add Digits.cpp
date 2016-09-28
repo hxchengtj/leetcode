c778079af3031e4b2e681674c7ad5fcf7df37891
ziyeqinghan <ziyeqinghan@gmail.com>
2016-09-28 16:45:36 +0800

    redo Add Digits

--- Add Digits.cpp
+++ Add Digits.cpp
@@ -1,4 +1,11 @@
+/* review again */
 int addDigits(int num) {
   return (num-1)%9+1;
 }
 
+class Solution {
+public:
+    int addDigits(int num) {
+        return num==0?0:(num%9 == 0?9:num%9);
+    }
+};
