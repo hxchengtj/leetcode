f75bd0348aef3b31fa074739a67406d118abbaf0
ziyeqinghan <ziyeqinghan@gmail.com>
2016-10-01 22:24:46 +0800

    redo Longest Increasing Subsequence

--- Longest Increasing Subsequence.cpp
+++ Longest Increasing Subsequence.cpp
@@ -18,3 +18,24 @@ public:
         return ans;
     }
 };
+
+//version2
+//O(nlogn)
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size(), ans = 0;
+        vector<int> f(n, INT_MAX);
+        for(int i = 0; i < n; i++) {
+            int l = 0, r = i;
+            while(l <= r) {
+                int m = l + (r-l)/2;
+                if(f[m] < nums[i]) l = m+1;
+                else r = m-1;
+            }
+            f[l] = nums[i];
+            ans = max(ans, l+1);
+        }
+        return ans;
+    }
+};
